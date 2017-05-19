06ac7b31bbcfc090ba8340853876e141c530941b
mahesh <mahesh.perla@gritinnovatino.com>
2017-05-19 14:55:11 +0530

    adding the remianing files

---
+++ app-controller.js
@@ -0,0 +1,11 @@
+'use strict';
+
+appModule
+
+.controller('mainAppCtrl', function($scope, $location) {
+        
+    // $scope.burl = $location.protocol() + '://' + $location.host() + ':' + $location.port();
+    $location.path('/red');
+})
+
+;
---
+++ app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var appModule = angular.module('sample-app', [
+        'ngRoute'
+    ]);
+
+// app.constant('appConst', {
+//         PROJECT_MANAGER: 'Project Manager'
+//     });
+
+appModule.config(function($routeProvider) {
+    $routeProvider
+    .when("/red", {
+        templateUrl : "./modules/red/red.html",
+        controller: "redController"
+    })
+    .when("/green", {
+        templateUrl : "modules/green/green.html"
+    });
+});
